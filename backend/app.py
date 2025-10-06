5c4ceba9ca807a81234e83c78967b1c3848e859e
Gizem Gürsoy <ggizemggursoy@gmail.com>
2025-10-06 12:50:00 +0300

    Add backend folder with LFS tracking

---
+++ backend/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d20dd7cfbe27775ae824545744f1d25816cef316261d2e0d0d267ae55174c40e
+size 5425
---
+++ backend/model_pipeline.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a702cc98fe1ff493cb7ac8fffd7a7870e2cb173333470304f3432c7a9a55b522
+size 1945
